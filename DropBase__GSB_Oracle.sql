fa81d9412d31b0aee32b4b53fb292675cd4ec418
laktati <anas_x96@hotmail.fr>
2015-11-19 14:37:57 +0100

    no message

---
+++ DropBase__GSB_Oracle.sql
@@ -0,0 +1,32 @@
+-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+-- Script généré le 14/11/2013 - SGBD cible : Oracle version 10
+-- Génération d'un script SQL à partir d'une base Access V 1.0 - Pierre Loisel - CERTA
+-- Script SQL de suppression de la base de données générée par le script F:\DATA\Jol1213\SIO\PPE4\Application Bureau GSB Java\Existant\CreateBase_new.sql.
+-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+drop table "ACTIVITE_COMPL" cascade constraints;
+drop table "COMPOSANT" cascade constraints;
+drop table "CONSTITUER" cascade constraints;
+drop table "DOSAGE" cascade constraints;
+drop table "FAMILLE" cascade constraints;
+drop table "FORMULER" cascade constraints;
+drop table "INTERAGIR" cascade constraints;
+drop table "INVITER" cascade constraints;
+drop table "LABO" cascade constraints;
+drop table "MEDICAMENT" cascade constraints;
+drop table "OFFRIR" cascade constraints;
+drop table "POSSEDER" cascade constraints;
+drop table "PRATICIEN" cascade constraints;
+drop table "PRESCRIRE" cascade constraints;
+drop table "PRESENTATION" cascade constraints;
+drop table "RAPPORT_VISITE" cascade constraints;
+drop table "REALISER" cascade constraints;
+drop table "REGION" cascade constraints;
+drop table "SECTEUR" cascade constraints;
+drop table "SPECIALITE" cascade constraints;
+drop table "Switchboard Items" cascade constraints;
+drop table "TRAVAILLER" cascade constraints;
+drop table "TYPE_INDIVIDU" cascade constraints;
+drop table "TYPE_PRATICIEN" cascade constraints;
+drop table "VISITEUR" cascade constraints;
+drop sequence "auto_ACTIVITE_COMPL";
+drop sequence "auto_RAPPORT_VISITE";
