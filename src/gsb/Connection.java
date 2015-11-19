bb5118739a88eb49269b9d414a46e047bd2b9955
laktati <anas_x96@hotmail.fr>
2015-11-19 17:29:12 +0100

    no message

---
+++ src/gsb/Connection.java
@@ -0,0 +1,98 @@
+package gsb;
+
+public class Connection extends javax.swing.JFrame {
+
+    /**
+     * Creates new form NewJFrame
+     */
+    public Connection() {
+        initComponents();
+    }
+
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("RAPPORT_VISITE");
+        setPreferredSize(new java.awt.Dimension(750, 400));
+        setResizable(false);
+        getContentPane().setLayout(null);
+
+        jLabel1.setText("Identifiez vous :");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(80, 60, 90, 30);
+
+        jButton1.setText("OK");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(230, 120, 47, 23);
+
+        jButton2.setText("Quitter");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(620, 330, 67, 23);
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(70, 100, 140, 30);
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(70, 140, 140, 30);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gsb/Logo_656853.jpg"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(-30, 20, 770, 370);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Connection().setVisible(true);
+                
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    // End of variables declaration//GEN-END:variables
+}
