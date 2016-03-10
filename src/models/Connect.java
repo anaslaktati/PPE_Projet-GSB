6b1e09be0dcdf034b4ab5d0d0458cb17154e1167
laktati <anas_x96@hotmail.fr>
2016-03-10 15:20:05 +0100

    no message

---
+++ src/models/Connect.java
@@ -0,0 +1,24 @@
+package models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connect {
+
+  public static Connection Connection() throws SQLException, ClassNotFoundException {
+    
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      
+      String url = "jdbc:oracle:thin:172.15.11.102/btssio@//localhost:1521/xe";
+      String user = "btssio";
+      String password = "btssio";
+
+      Connection con = DriverManager.getConnection(url, user, password);
+
+      return con;
+      
+  }
+}
--- src/vues/Connection.java
+++ src/vues/Connection.java
@@ -1,117 +1,34 @@
-package vues;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vues;
 
-import controleurs.Controleur;
-import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+/**
+ *
+ * @author Anas
+ */
 
-public class Connection extends javax.swing.JFrame {
+public class connection {
+    //CTRL + SHIFT + O pour générer les imports
+    public static void main(String[] args) {      
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            System.out.println("Driver OK");
 
-    /**
-     * Creates new form NewJFrame
-     */
-    
-    private Controleur controleur;
-    
-    public Connection() {
-        initComponents();
-    }
+            String url = "jdbc:oracle:thin:@172.15.11.102:1521:orcl";
+            String user = "ora_2slamppe_eq2";
+            String password = "equipe02";
 
-    public Controleur getControleur() {
-          return controleur;
-      }
-    
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection réussis !");
 
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("RAPPORT_VISITE");
-        setPreferredSize(new java.awt.Dimension(750, 400));
-        setResizable(false);
-        getContentPane().setLayout(null);
-
-        jLabel1.setText("Identifiez vous :");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(80, 60, 90, 30);
-
-        jButton1.setText("OK");
-        getContentPane().add(jButton1);
-        jButton1.setBounds(230, 120, 47, 23);
-
-        jButton2.setText("Quitter");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton2);
-        jButton2.setBounds(620, 330, 67, 23);
-        getContentPane().add(jTextField1);
-        jTextField1.setBounds(70, 100, 140, 30);
-        getContentPane().add(jTextField2);
-        jTextField2.setBounds(70, 140, 140, 30);
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vues/Logo_656853.jpg"))); // NOI18N
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(-30, 20, 770, 370);
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        getControleur.
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Connection.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Connection().setVisible(true);
-                
-            }
-        });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    // End of variables declaration//GEN-END:variables
 }
